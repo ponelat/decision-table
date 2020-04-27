26d385cc0bba3a0c233a1da11f3dd27c68191717
Josh Ponelat <jponelat@gmail.com>
2020-04-28 00:15:12 +0200

    Updates

---
+++ precache-manifest.919ce6c65cc7d9b860f5570dfaac7bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53be0a15e2bd745b29a59722d8676b9",
+    "url": "/decision-table/index.html"
+  },
+  {
+    "revision": "fdecc9febf0825b4c73f",
+    "url": "/decision-table/static/css/main.f181cae8.chunk.css"
+  },
+  {
+    "revision": "104ccab6a155a0008b6c",
+    "url": "/decision-table/static/js/2.58ea0fb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/decision-table/static/js/2.58ea0fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdecc9febf0825b4c73f",
+    "url": "/decision-table/static/js/main.cfdc7d7f.chunk.js"
+  },
+  {
+    "revision": "43489d88150bcd81ba02",
+    "url": "/decision-table/static/js/runtime-main.17281230.js"
+  }
+]);
